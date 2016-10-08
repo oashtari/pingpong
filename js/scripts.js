71d316821f9a6fb257b64dde71ab0da20e8d3140
Omid Ashtari <oashtari@gmail.com>
2016-10-08 00:12:51 -0700

    add a variety of changes to the js script, and minor modifications to the html doc

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,50 @@
 // Back-end
 var input;
-var number;
+var number = [];
+var ping;
+var pong;
+var pingpong;
 
-var numbers = function () {
+var pp = function (input) {
+  if (input > 250) {
+    alert("Please enter a smaller number, a number <= 250 - thank you!")
+  } else {
+    for (i=1; i<=input; i+=1) {
+      number.push(i);
+      number.forEach(function(numbers){
+        if (numbers % 15 === 0) {
+          number.replace(numbers,"pingpong");
+        } else if (numbers % 5 === 0) {
+          number.replace(numbers,"pong");
+        } else if (numbers % 3 === 0) {
+          ping = number.slice(numbers);
+          number = number.replace(ping,"ping");
+        } else {
+          return (numbers)
+          }
+})
+}
+}
+}
 
-if (number > 250) {
-  alert("Please enter a smaller number, a number <= 250 - thank you!")
-} else if (for (i=0; i<=number; i+=1)) {
-  } if (i % 15 === 0) {
-    print ("pingpong");
-  } else if (i % 5 === 0) {
-      print ("pong");
-    } else if (i % 3 === 0) {
-      print ("ping");
-    } else if {
-      print (number)
-    }
-  }
-//
-// array.forEach(function(soap) {
-//       $("#omid").append("<p>" + soap + "</p>");
-//     })
-
-
-
-
+// else if (words.charAt(0) !== (vowel)) {
+//           firstLetter = words.slice(0,1);
+//           newWords = words.replace(words.charAt(0),'') + firstLetter + "ay";
 
 // Front-end
 $(document).ready(function(){
   $("form#number").submit(function(event){
     event.preventDefault();
     input = parseInt($("input#number").val());
-    number = input + 1;
+    pp(input);
+    $("#result").show();
+
   })
-})
+});
 
-
-
+// $("#result").append("<li>" + "pingpong" + "</li>");
+// $("#result").append("<li>" + "pong" + "</li>");
+//   $("#result").append("<li>" + "ping" + "</li>");
 
 
 // A user should be able to enter a new number and see new results over and over again.
