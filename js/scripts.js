593c6caa815c60f323014e6689d38f1d44d8c115
Omid Ashtari <oashtari@gmail.com>
2016-10-09 13:14:29 -0700

    add fix so page deletes old info, deleted unnecessary CSS

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,12 @@
-// Back-end
+// BACK-END
 var input;
 var number = [];
-// var pingpong = [];
 
 var pp = function (input) {
   if (input > 250) {
     alert("Please enter a smaller number, a number <= 250 - thank you!")
   } else {
-    for (i=1; i<=input; i+=1) {
+    for (i=0; i<=input; i+=1) {
       number.push(i);
 }
 }
@@ -15,7 +14,9 @@ var pp = function (input) {
 
 var pingpong = function(){
     number.map(function(numbers){
-    if (numbers % 15 === 0) {
+    if (numbers === 0) {
+      $("#result").append("<li>" + "Enjoy:" + "</li>");
+    } else if (numbers % 15 === 0) {
       $("#result").append("<li>" + "pingpong" + "</li>");
     } else if (numbers % 5 === 0) {
       $("#result").append("<li>" + "pong" + "</li>");
@@ -24,27 +25,23 @@ var pingpong = function(){
     } else {
       $("#result").append("<li>" + numbers + "</li>");
       }
-  })
   }
+)}
 
 
-// else if (words.charAt(0) !== (vowel)) {
-//           firstLetter = words.slice(0,1);
-//           newWords = words.replace(words.charAt(0),'') + firstLetter + "ay";
-
-// Front-end
+// FRONT-END
 $(document).ready(function(){
   $("form#number").submit(function(event){
     event.preventDefault();
     input = parseInt($("input#number").val());
+    $("#result").empty();
+    number = [];
     pp(input);
     pingpong();
-    $("#result").show();
+    // $("#result").show();
+    $('#result').click(function() {
+        location.reload();
+
   });
 });
-
-
-
-
-// A user should be able to enter a new number and see new results over and over again.
-//
+});
